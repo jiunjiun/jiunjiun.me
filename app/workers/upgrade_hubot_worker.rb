a8145fcf5205d00933c6a775bf25affe672023ff
jiunjiun <quietmes@gmail.com>
2015-08-11 01:29:52 +0800

    fix git pull

--- app/workers/upgrade_hubot_worker.rb
+++ app/workers/upgrade_hubot_worker.rb
@@ -3,7 +3,7 @@ class UpgradeHubotWorker
   include Sidekiq::Status::Worker
 
   def perform
-    cmd = 'cd /home/rails/git/sinlead-hubot; git push heroku master'
+    cmd = 'cd /home/rails/git/sinlead-hubot; git pull; git push heroku master'
     # cmd = 'cd /Users/jiun/git/sinlead-hubot; git push heroku master'
     `#{cmd}`
   end
