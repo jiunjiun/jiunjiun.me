cc67f2551c3a753c6fcf2eadaacab3b39daddd02
jiunjiun <quietmes@gmail.com>
2015-08-11 01:34:07 +0800

    test cmd

--- app/services/upgrade_hubot.rb
+++ app/services/upgrade_hubot.rb
@@ -1,5 +1,8 @@
 class UpgradeHubot
   def self.sinlead
-    UpgradeHubotWorker.perform_async
+    # UpgradeHubotWorker.perform_async
+    cmd = 'cd /home/rails/git/sinlead-hubot; git pull; git push heroku master'
+    # cmd = 'cd /Users/jiun/git/sinlead-hubot; git push heroku master'
+    `#{cmd}`
   end
 end
